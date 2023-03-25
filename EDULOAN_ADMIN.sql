1638f1eebb57b04f1cff0b9a38f2b269512ee9ed
PratikThoratNortheastern <thorat.p@northeastern.edu>
2023-03-25 18:11:18 -0400

    Added Views

--- EDULOAN_ADMIN.sql
+++ EDULOAN_ADMIN.sql
@@ -406,3 +406,78 @@ VALUES (REPAYMENT_ID_SEQ.NEXTVAL, 9, TO_DATE('2022-09-01', 'YYYY-MM-DD'), 1000);
 
 INSERT INTO loan_repayments (repayment_id, application_id, payment_date, amount_paid)
 VALUES (REPAYMENT_ID_SEQ.NEXTVAL, 10, TO_DATE('2022-10-01', 'YYYY-MM-DD'), 3000);
+
+--VIEWS
+
+CREATE OR REPLACE VIEW loan_details_view AS
+SELECT 
+    la.first_name || ' ' || la.last_name AS applicant_name,
+    la.email AS applicant_email,
+    la.phone_number AS applicant_phone,
+    lo.first_name || ' ' || lo.last_name AS loan_officer_name,
+    la.created_at AS application_date,
+    laa.amount_requested AS requested_amount,
+    laa.amount_approved AS approved_amount,
+    laa.loan_term AS loan_term,
+    laa.interest_rate AS interest_rate,
+    laa.status AS application_status,
+    ld.date_disbursed AS disbursement_date,
+    ld.amount_disbursed AS disbursed_amount,
+    lr.payment_date AS repayment_date,
+    lr.amount_paid AS paid_amount
+FROM 
+    loan_applicants la
+    JOIN loan_applications laa ON la.applicant_id = laa.applicant_id
+    JOIN loan_officers lo ON laa.officer_id = lo.officer_id
+    LEFT JOIN loan_disbursements ld ON laa.application_id = ld.application_id
+    LEFT JOIN loan_repayments lr ON laa.application_id = lr.application_id;
+    
+
+-- SELECT * FROM loan_details_view;
+
+CREATE OR REPLACE VIEW disbursements_by_officer_view AS
+SELECT loan_officers.first_name || ' ' || loan_officers.last_name AS officer_name,
+       loan_applications.applicant_id,
+       loan_applicants.first_name || ' ' || loan_applicants.last_name AS applicant_name,
+       loan_applications.amount_approved AS loan_amount,
+       loan_disbursements.date_disbursed,
+       SUM(loan_disbursements.amount_disbursed) AS amount_disbursed
+FROM loan_officers
+JOIN loan_applications ON loan_officers.officer_id = loan_applications.officer_id
+JOIN loan_applicants ON loan_applications.applicant_id = loan_applicants.applicant_id
+JOIN loan_disbursements ON loan_applications.application_id = loan_disbursements.application_id
+GROUP BY loan_officers.first_name, loan_officers.last_name, loan_applications.applicant_id, loan_applicants.first_name, loan_applicants.last_name,
+         loan_applications.amount_approved, loan_disbursements.date_disbursed;
+
+-- SELECT * FROM disbursements_by_officer_view;
+
+CREATE OR REPLACE VIEW repayments_by_applicant_view AS
+SELECT loan_applicants.first_name || ' ' || loan_applicants.last_name AS applicant_name,
+       loan_applications.application_id,
+       loan_applications.amount_approved AS loan_amount,
+       loan_repayments.payment_date,
+       loan_repayments.amount_paid,
+       loan_applications.amount_approved - SUM(loan_repayments.amount_paid) AS outstanding_balance
+FROM loan_applicants
+JOIN loan_applications ON loan_applicants.applicant_id = loan_applications.applicant_id
+JOIN loan_repayments ON loan_applications.application_id = loan_repayments.application_id
+GROUP BY loan_applicants.first_name, loan_applicants.last_name, loan_applications.application_id,
+         loan_applications.amount_approved, loan_repayments.payment_date, loan_repayments.amount_paid;
+
+
+-- SELECT * FROM repayments_by_applicant_view;
+
+CREATE OR REPLACE VIEW loan_officer_view AS
+SELECT 
+    loan_officers.first_name || ' ' || loan_officers.last_name AS officer_name,
+    COUNT(loan_applications.application_id) AS num_applications_processed,
+    SUM(loan_applications.amount_approved) AS total_loan_amount_approved
+FROM 
+    loan_officers
+LEFT JOIN 
+    loan_applications ON loan_officers.officer_id = loan_applications.officer_id
+GROUP BY 
+    loan_officers.officer_id,
+    loan_officers.first_name,
+    loan_officers.last_name;
+
